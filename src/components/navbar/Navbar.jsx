638f9303ae2d546003fbd45372f5879161cf326e
Reisel Valle <reiselvalle@gmail.com>
2023-11-17 15:25:39 -0500

    installed router

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Navbar() {
+  return (
+    <div className="p-5 pb-0 flex flex-wrap justify-center sm:justify-between items-center
+                    border-b dark:border-gray-700 border-gray-200">
+      <div className="flex justify-between items-center space-x-5 w-screen">
+        Search-App
+      </div>
+    </div>
+  )
+}
