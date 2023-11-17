47eeeb764750d89bd7e4eb574cb022da7b075bf9
Reisel Valle <reiselvalle@gmail.com>
2023-11-17 15:24:01 -0500

    modified App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import { useState } from 'react';
+import Navbar from './components/navbar/Navbar';
+import Routes from './components/routes/Routes';
+import Footer from './components/footer/Footer';
 
 function App() {
+    const [darkTheme, setDarkTheme] = useState(false);
 
   return (
-    <>
-      <h1 className="text-3xl font-bold">Search</h1>
-    </>
+    <div className={ darkTheme ? 'dark' : ''}>
+       <div className='bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 min-h-screen'>
+        <Navbar />
+        <Routes />
+        <Footer />
+       </div>
+    </div>
   )
 }
 
