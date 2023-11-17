f6ef0497c20aff7dc9a706842501dd9abcc51e73
Reisel Valle <reiselvalle@gmail.com>
2023-11-17 17:02:11 -0500

    used </ResultContextProvider>

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { ResultContextProvider } from './context/ResultContextProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <ResultContextProvider>
+        <App />
+      </ResultContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
