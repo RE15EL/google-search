71c01e0d8419acc2b217e082f9107ad4adb18f21
Reisel Valle <reiselvalle@gmail.com>
2023-11-17 16:15:30 -0500

    created footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,6 +2,8 @@
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className="text-center p-10 border-t dark:border-gray-700 border-gray-200">
+      <h1>2023 Reyy, Inc.</h1>
+    </div>
   )
 }
